beaed9e80ac6496a02b0d25b3ac8437f5da1797b
YooSejeong <nived3@naver.com>
2025-05-15 23:38:33 +0900

    @Query, DTO 조회, 파라미터 바인딩, 순수 JPA 페이징과 정렬

--- data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -1,14 +1,20 @@
 package study.data_jpa.repository;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
+import study.data_jpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -17,6 +23,8 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
     public void testMember() {
@@ -25,8 +33,129 @@ class MemberRepositoryTest {
         Member savedMember = memberRepository.save(member);
         Member findMember = memberRepository.findById(savedMember.getId()).get();
 
-        Assertions.assertThat(findMember.getId()).isEqualTo(savedMember.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(savedMember);
+        assertThat(findMember.getId()).isEqualTo(savedMember.getId());
+        assertThat(findMember.getUsername()).isEqualTo(savedMember.getUsername());
+        assertThat(findMember).isEqualTo(savedMember);
     }
+
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
+    }
+
+    @Test
+    public void findByUsernameAndAgeGreaterThan() {
+        Member member1 = new Member("aaa", 10);
+        Member member2 = new Member("aaa", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("aaa", 15);
+
+        assertThat(result.get(0).getUsername()).isEqualTo("aaa");
+        assertThat(result.get(0).getAge()).isEqualTo(20);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void findHelloBy() {
+        List<Member> helloBy = memberRepository.findHelloBy();
+    }
+
+    @Test
+    public void testQuery() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 29);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        assertThat(result.get(0)).isEqualTo(member1);
+    }
+
+    @Test
+    public void findUsernameList() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 29);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<String> usernameList = memberRepository.findUsernameList();
+
+        for (String s : usernameList) {
+            System.out.println("member : " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 29);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Team teamA = new Team("teamA");
+        teamRepository.save(teamA);
+
+        member1.setTeam(teamA);
+
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto : " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 29);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        for (Member member : result) {
+            System.out.println("member = " + member);
+        }
+    }
+
+    @Test
+    public void returnType() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("BBB", 29);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findListByUsername("AsdfeAA");
+        System.out.println("result = " + result.size());
+    }
+
+
 }
